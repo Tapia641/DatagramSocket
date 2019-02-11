65f0d0b1eb3a906f992e498f7585971b3831917b
tapia <tapia641@gmail.com>
2019-02-11 07:48:48 -0600

    Terminado

--- ClienteDatagramaUDP.java
+++ ClienteDatagramaUDP.java
@@ -11,18 +11,26 @@ public class ClienteDatagramaUDP {
 
     public static void iniciarCliente() {
         try {
+
+            /* CREAMOS EL DATAGRAMSOCKET, PUERTO DESDE DONDE VOY A ENVIAR (NO IMPORTA) */
             DatagramSocket clienteSocket = new DatagramSocket();
             System.out.println("Cliente iniciado con exito ...");
+
+            /* ARREGLO ESTANDAR PARA ESCRIBIR MENSAJE */
             BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Ingresa un mensaje");
             String mensaje = teclado.readLine();
             byte[] b = mensaje.getBytes();
+
+            /* ENVIANDO AL LOCALHOST CON PUERTO 2000 (DONDE ESTA ESCUCHANDO) */
             String destino = "127.0.0.1";
             int port = 2000;
 
+            /* CREO EL PAQUETE, Y LO ENVIO CON EL SOCKET */
             DatagramPacket packet = new DatagramPacket(b,b.length, InetAddress.getByName(destino), port);
             clienteSocket.send(packet);
             clienteSocket.close();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
--- ServidorDatagramaUDP.java
+++ ServidorDatagramaUDP.java
@@ -8,15 +8,21 @@ public class ServidorDatagramaUDP {
 
     public static void iniciarServidor() {
         try {
-            /* INICIAMOS EL SERVIDOR CON PUERTO 2000 */
+            /* INICIAMOS EL SERVIDOR CON PUERTO PARA ESCUCHAR 2000 */
             DatagramSocket servidor = new DatagramSocket(2000);
             System.out.println("Servidor iniciado...");
 
             while (true) {
+                /* SOPORTA HASTA UN ARREGLO DE 2000 BYTES */
                 DatagramPacket packet = new DatagramPacket(new byte[2000], 2000);
+
                 /* BLOQUEO */
                 servidor.receive(packet);
+
+                /* DATOS DEL PAQUETE */
                 System.out.println("Datagrama recibido desde " + packet.getAddress() + " : " + packet.getPort());
+
+                /* LO CONVERTIMOS A UN STRING */
                 String mensaje = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("Con el mensaje: " + mensaje);
             }
