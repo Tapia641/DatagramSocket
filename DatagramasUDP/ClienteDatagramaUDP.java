9ae15841f06308210201d52dd592e86251765341
tapia <tapia641@gmail.com>
2019-02-23 01:52:34 -0600

    Update

--- DatagramasUDP/ClienteDatagramaUDP.java
+++ DatagramasUDP/ClienteDatagramaUDP.java
@@ -3,6 +3,8 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.io.DataOutputStream;
+import java.io.ByteArrayOutputStream;
 
 public class ClienteDatagramaUDP {
     public static void main(String[] args) {
@@ -12,23 +14,57 @@ public class ClienteDatagramaUDP {
     public static void iniciarCliente() {
         try {
 
-            /* CREAMOS EL DATAGRAMSOCKET, PUERTO DESDE DONDE VOY A ENVIAR (NO IMPORTA) */
+            /* CREAMOS EL DATAGRAMSOCKET, EL PUERTO DESDE DONDE VOY A ENVIAR (NO IMPORTA) */
             DatagramSocket clienteSocket = new DatagramSocket();
             System.out.println("Cliente iniciado con exito ...");
-
-            /* ARREGLO ESTANDAR PARA ESCRIBIR MENSAJE */
-            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Ingresa un mensaje");
-            String mensaje = teclado.readLine();
-            byte[] b = mensaje.getBytes();
 
             /* ENVIANDO AL LOCALHOST CON PUERTO 2000 (DONDE ESTA ESCUCHANDO) */
             String destino = "127.0.0.1";
             int port = 2000;
 
+            /* ARREGLO ESTANDAR PARA ESCRIBIR MENSAJE */
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Ingresa un mensaje: ");
+            String mensaje = teclado.readLine();
+
+            ByteArrayOutputStream arregloSalida = new ByteArrayOutputStream();
+            DataOutputStream dataSalida = new DataOutputStream(arregloSalida);
+
+            /* ENVIAMOS DATOS EN EL MISMO ORDEN QUE SE RECIBEN */
+            dataSalida.writeInt(mensaje.length());
+            dataSalida.flush();
+
+            byte[] c = arregloSalida.toByteArray();
+
+            DatagramPacket newpacket = new DatagramPacket(c, c.length, InetAddress.getByName(destino), port);
+            clienteSocket.send(newpacket);
+
             /* CREO EL PAQUETE, Y LO ENVIO CON EL SOCKET */
-            DatagramPacket packet = new DatagramPacket(b, b.length, InetAddress.getByName(destino), port);
-            clienteSocket.send(packet);
+            String part = "";
+            char[] caracteres = mensaje.toCharArray();
+
+            for (int i = 0, j = 0; i < caracteres.length; i++, j++) {
+
+                part += caracteres[i];
+
+                if (j == 19) {
+                    j = 0;
+                    byte[] b = part.getBytes();
+                    DatagramPacket packet = new DatagramPacket(b, b.length, InetAddress.getByName(destino), port);
+                    clienteSocket.send(packet);
+                    part = "";
+                }
+            }
+
+            /* SI SOBRAN DATOS POR ENVIAR, ENTONCES ENVIAMOS EL RESIDUO */
+            if (part.length() > 2) {
+                byte[] b = part.getBytes();
+                DatagramPacket packet = new DatagramPacket(b, b.length, InetAddress.getByName(destino), port);
+                clienteSocket.send(packet);
+            }
+
+            /* CERRAMOS LOS FLUJOS EN EL MISMO ORDEN */
+            dataSalida.close();
             clienteSocket.close();
 
         } catch (Exception e) {
--- DatagramasUDP/ServidorDatagramaUDP.java
+++ DatagramasUDP/ServidorDatagramaUDP.java
@@ -1,5 +1,7 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
 
 public class ServidorDatagramaUDP {
     public static void main(String[] args) {
@@ -7,13 +9,15 @@ public class ServidorDatagramaUDP {
     }
 
     public static void iniciarServidor() {
+
         try {
+
             /* INICIAMOS EL SERVIDOR CON PUERTO PARA ESCUCHAR 2000 */
             DatagramSocket servidor = new DatagramSocket(2000);
             System.out.println("Servidor iniciado...");
 
             while (true) {
-                
+
                 /* SOPORTA HASTA UN ARREGLO DE 2000 BYTES */
                 DatagramPacket packet = new DatagramPacket(new byte[2000], 2000);
 
@@ -23,9 +27,36 @@ public class ServidorDatagramaUDP {
                 /* DATOS DEL PAQUETE */
                 System.out.println("Datagrama recibido desde " + packet.getAddress() + " : " + packet.getPort());
 
-                /* LO CONVERTIMOS A UN STRING */
-                String mensaje = new String(packet.getData(), 0, packet.getLength());
-                System.out.println("Con el mensaje: " + mensaje);
+                /* CREANDO UN FLUJO SIN LIGAR AL SOCKET, SOLOA LOS DATOS RECIBIDOS */
+                DataInputStream destino = new DataInputStream(new ByteArrayInputStream(packet.getData()));
+
+                /* PREPARAMOS LOS DATOS A LEER DATOS PRIMITIVOS */
+                int tam = destino.readInt();
+
+                System.out.println("Tam: " + tam);
+
+                String mensaje, part = "";
+
+                while (part.length() != tam) {
+
+                    /* BLOQUEO, RECIBIMOS EL PAQUETE */
+                    servidor.receive(packet);
+
+                    /* DATOS DEL PAQUETE */
+                    //System.out.println("Datagrama recibido desde " + packet.getAddress() + " : " + packet.getPort());
+
+                    /* LO CONVERTIMOS A UN STRING */
+                    mensaje = new String(packet.getData(), 0, packet.getLength());
+                    System.out.println("Mensaje: " + mensaje);
+
+                    /* CONCATENAMOS EL PAQUETE RECIBIDO PARA RECONSTRUIR EL MENSAJE */
+                    part += mensaje;
+                    //System.out.println("Tam de part: " + part.length());
+
+                }
+
+                System.out.println("Mensaje reconstruido: " + part);
+
             }
         } catch (Exception e) {
             e.printStackTrace();
